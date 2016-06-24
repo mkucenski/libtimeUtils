973d1c36ddfabe52a813b14a2ee9d0a44af0e56b
mkucenski <mkucenski@me.com>
2016-06-24 07:12:09 -0600

    Update timeZoneCalculator.h
    Fixing comment spacing for readability

--- src/timeZoneCalculator.h
+++ src/timeZoneCalculator.h
@@ -24,34 +24,34 @@ using namespace boost;
 class timeZoneCalculator {
 	public:
 		timeZoneCalculator();													//Default to GMT
-		timeZoneCalculator(string strPosixTimeZone);						//Select time zone from a POSIX time zone string
-		timeZoneCalculator(string strRegion, string strFilePath);		//Select time zone by region name from a time zone database
+		timeZoneCalculator(string strPosixTimeZone);			//Select time zone from a POSIX time zone string
+		timeZoneCalculator(string strRegion, string strFilePath);	//Select time zone by region name from a time zone database
 		~timeZoneCalculator();
 
-		int setTimeZone(string strPosixTimeZone);							//Select time zone from a POSIX time zone string
-		int setTimeZone(string strRegion, string strFilePath);			//Select time zone by region name from a time zone database
-		local_time::time_zone_ptr getTimeZone() { return m_TZ; };		//Retrieve a copy of the local_time::time_zone_ptr member
+		int setTimeZone(string strPosixTimeZone);			//Select time zone from a POSIX time zone string
+		int setTimeZone(string strRegion, string strFilePath);		//Select time zone by region name from a time zone database
+		local_time::time_zone_ptr getTimeZone() { return m_TZ; };	//Retrieve a copy of the local_time::time_zone_ptr member
 		
-		local_time::local_date_time calculateLocalTime(posix_time::ptime pt);			//Calculate local time from a UTC ptime
-																										//value based on the loaded time zone.
-																											
-		local_time::local_date_time createLocalTime(	gregorian::date d, 					//Create a local_date_time object from local
-																posix_time::time_duration td);	//date and time values that are already in 
+		local_time::local_date_time calculateLocalTime(posix_time::ptime pt);	//Calculate local time from a UTC ptime
+											//value based on the loaded time zone.
+											
+		local_time::local_date_time createLocalTime(	gregorian::date d, 		//Create a local_date_time object from local
+								posix_time::time_duration td);	//date and time values that are already in 
 																										//this time zone.
 																											
-		local_time::local_date_time createLocalTime(	u_int8_t month, 					//Create a local_date_time object from month,
-																u_int8_t day, 						//day, year, hour, min, sec values that are 
-																u_int16_t year, 					//already in this time zone.
-																u_int8_t hour, 
-																u_int8_t min, 
-																u_int8_t sec);						
+		local_time::local_date_time createLocalTime(	u_int8_t month, //Create a local_date_time object from month,
+								u_int8_t day, 	//day, year, hour, min, sec values that are 
+								u_int16_t year, //already in this time zone.
+								u_int8_t hour, 
+								u_int8_t min, 
+								u_int8_t sec);
 																	
-		local_time::local_date_time createLocalTime(string str, string strfmt);		//Create a local_date_time object from a 
-																										//date/time string that is already in this 
-																										//time zone.
+		local_time::local_date_time createLocalTime(string str, string strfmt);	//Create a local_date_time object from a 
+											//date/time string that is already in this 
+											//time zone.
 		
-		string getTimeZoneString();											//Return the POSIX string representation of the currently 
-																					//loaded time zone.
+		string getTimeZoneString();	//Return the POSIX string representation of the currently 
+						//loaded time zone.
 		
 	private:
 		local_time::time_zone_ptr m_TZ;
